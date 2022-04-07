e60d6af4bdc846c7a4a3e08e7cd5b33111c016e7
hjmduarte <99761308+hjmduarte@users.noreply.github.com>
2022-04-07 15:48:16 -0400

    update btn administradores

---
+++ controlers.js
@@ -0,0 +1,4 @@
+const btnForm = document.querySelector("#btn-form");
+ const form = document.querySelector("#section-form");
+  btnForm.addEventListener("click", function(){ 
+      form.classList.toggle("desplegar")})
